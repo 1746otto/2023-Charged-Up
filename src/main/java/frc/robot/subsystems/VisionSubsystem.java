9d3bc998351ef22bbb3bb7fc63072961754434f0
Carson Walker <orangecw10@gmail.com>
2023-01-30 20:04:07 -0500

    switched to usb

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -5,23 +5,31 @@
 package frc.robot.subsystems;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.lang.Integer;
 
 public class VisionSubsystem extends SubsystemBase {
-  private SPI openMV;
-  private byte[] bytes = new byte[4];
+  //private SPI openMVSPI;
+  private static final int bytesTransfered = 4;
+  private SerialPort openMV;
+  private byte[] bytes = new byte[bytesTransfered];
+  private int[] buffer = new int[1];
   /** Creates a new ExampleSubsystem. */
   public VisionSubsystem() {
-    openMV = new SPI(Port.kOnboardCS0);
-    openMV.setClockRate(4000000);//4mil
-    openMV.setChipSelectActiveLow();
-    openMV.setMode(SPI.Mode.kMode2);
+    // openMVSPI = new SPI(Port.kOnboardCS0);
+    // openMVSPI.setClockRate(640000);//4mil
+    // openMVSPI.setChipSelectActiveLow();
+    // openMVSPI.setMode(SPI.Mode.kMode2);
+    openMV = new SerialPort(9600, SerialPort.Port.kUSB1, 8);
+    openMV.setFlowControl(SerialPort.FlowControl.kRtsCts);
+    openMV.setTimeout(0.01);
     
   }
 
@@ -48,12 +56,25 @@ public class VisionSubsystem extends SubsystemBase {
     }
     return returnBytes;
   }
-
+  public String byteToHex(byte num) {
+    char[] hexDigits = new char[2];
+    hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+    hexDigits[1] = Character.forDigit((num & 0xF), 16);
+    return new String(hexDigits);
+}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    openMV.read(true, bytes, 4);
+    //openMV.read(true, bytes, 20);
+    // openMVSPI.read(true, bytes, 4);
+    if (openMV.getBytesReceived() == bytesTransfered) {
+      bytes = openMV.read(bytesTransfered);
+    }
     System.out.println(ByteBuffer.wrap(bytes).getFloat());
+    // System.out.print(byteToHex(bytes[0]));
+    // System.out.print(byteToHex(bytes[1]));
+    // System.out.print(byteToHex(bytes[2]));
+    // System.out.println(byteToHex(bytes[3]));
   }
 
   @Override
