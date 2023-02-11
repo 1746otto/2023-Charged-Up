c8d0ed709836f618fac0815d08adf8650fd35595
Srinikesh <legendary81001@gmail.com>
2023-02-11 14:18:21 -0500

    no error placer

--- src/main/java/frc/robot/commands/PlacerHorizontalCommand.java
+++ src/main/java/frc/robot/commands/PlacerHorizontalCommand.java
@@ -3,11 +3,24 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.PlacerHorizontalSubsystem;
 
-public class PlacerHorizontalCommand {
-    private final PlacerHorizontalSubsystem m_placer;
+public class PlacerHorizontalCommand extends CommandBase{
+    private final PlacerHorizontalSubsystem m_placer2;
     public PlacerHorizontalCommand(PlacerHorizontalSubsystem subsystem) {
-        m_placer = subsystem;
+        m_placer2 = subsystem;
         addRequirements(subsystem);
-       
+    }
+    private void addRequirements(PlacerHorizontalSubsystem subsystem) {
+    }
+    @Override
+    public void initialize() {
+        m_placer2.extendPlacerIn();
+    }
+    @Override
+    public void end(boolean interrupted) {
+        m_placer2.DisengagePlacer();
+    }
+    @Override
+    public boolean isFinished() {
+        return !m_placer2.isAtSide();
     }
 }
