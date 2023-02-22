9896d30b0fc98eeb8d006f7eaebeec5f5fce16aa
Josselyn Trejo <josselyntrejo1@gmail.com>
2023-02-22 18:44:36 -0500

    started writing smart dashboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -46,6 +47,7 @@ public class RobotContainer {
     private final XboxController m_controller = new XboxController(ControllerConstants.kport);
     private final XboxController m_controller2 = new XboxController(ControllerConstants.kport2);
     private final Alliance allianceColor = DriverStation.getAlliance();
+    private final SendableChooser<Command> m_chooser = new SendableChooser<>();
   
   
     /* Controllers */
@@ -174,6 +176,13 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
         return new Auton(s_Swerve, allianceColor);
+        m_chooser.setDefaultOption("Auton1", ());
+        //m_chooser.addOption("Auton2", getAutonomousCommand());
     }
+
+
+
+
+    
 }
 
