15da96eb074b204ad15d13ea5b5c590f572aea72
Josselyn Trejo <josselyntrejo1@gmail.com>
2023-02-22 18:12:28 -0500

    Added alliance color into robot container

--- src/main/java/frc/robot/Autos/Auton.java
+++ src/main/java/frc/robot/Autos/Auton.java
@@ -13,11 +13,12 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 public class Auton extends SequentialCommandGroup{
-    public Auton(Swerve s_Swerve){
+    public Auton(Swerve s_Swerve, Alliance allianceColor){
         addCommands(new SequentialCommandGroup(new firstTrajectory(s_Swerve),new secondTrajectory(s_Swerve)));
     }
     
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
@@ -43,6 +45,7 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 public class RobotContainer {
     private final XboxController m_controller = new XboxController(ControllerConstants.kport);
     private final XboxController m_controller2 = new XboxController(ControllerConstants.kport2);
+    private final Alliance allianceColor = DriverStation.getAlliance();
   
   
     /* Controllers */
@@ -170,7 +173,7 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        return new Auton(s_Swerve);
+        return new Auton(s_Swerve, allianceColor);
     }
 }
 
