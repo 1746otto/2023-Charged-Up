8fea14cccc4de981f8d4ea6b7a28f10463dc04a4
community pc <yousarayu@gmail.com>
2023-02-25 16:59:54 -0500

    not finished

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,17 +4,49 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Swerve;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.commands.ScoringAlignCommand;
+import frc.robot.commands.ExampleCommand;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
-  }
+    Swerve m_swerve;
+    ScoringAlignCommand m_scoringAlignCommand;
 
-  private Autos() {
-    throw new UnsupportedOperationException("This is a utility class!");
-  }
+    public Autos(Swerve swerve, ScoringAlignCommand alignCommand) {
+        m_swerve = swerve;
+        m_scoringAlignCommand = alignCommand;
+    }
+
+    public Command exampleAuto() {
+        PathPlannerTrajectory exampleTrajectory = PathPlanner.loadPath("Example Path", new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared));
+        ArrayList<PathPlannerTrajectory> pathGroup = new ArrayList<PathPlannerTrajectory>();
+        
+        pathGroup.add(
+            PathPlanner.generatePath(
+                new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+                new PathPoint(new Translation2d(m_swerve.getPose().getX(), m_swerve.getPose().getY()), Rotation2d.fromDegrees(0), m_swerve.getPose().getRotation()),
+                new PathPoint(new Translation2d(exampleTrajectory.getInitialState().poseMeters.getX(), exampleTrajectory.getInitialState().poseMeters.getY()), 
+                exampleTrajectory.getInitialState().poseMeters.getRotation(), exampleTrajectory.getInitialState().holonomicRotation)
+            )
+        );
+        
+        return new SequentialCommandGroup(
+            
+        );
+    }
 }
