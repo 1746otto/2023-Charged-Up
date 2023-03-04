d62b2b546d6e61d27d49898b05c8f876afed99ed
Srinikesh <legendary81001@gmail.com>
2023-03-04 09:40:29 -0500

    code

--- src/main/java/frc/robot/subsystems/IntakeExtendSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeExtendSubsystem.java
@@ -1,46 +1,43 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-// import edu.wpi.first.wpilibj.DigitalInput;
-// import edu.wpi.first.wpilibj2.command.CommandBase;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants.IntakeExtendConstants;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeExtendConstants;
 
-// public class IntakeExtendSubsystem extends SubsystemBase {
-// /** Creates a new ExampleSubsystem. */
-//   CANSparkMax masterMotor;
-//   CANSparkMax slaveMotor;
-//   DigitalInput limitSwitch1;
-//   DigitalInput limitSwitch2;
+public class IntakeExtendSubsystem extends SubsystemBase {
+/** Creates a new ExampleSubsystem. */
+  CANSparkMax masterMotor;
+  DigitalInput limitSwitch1;
+  DigitalInput limitSwitch2;
 
 
-//   public IntakeExtendSubsystem() {
-//     masterMotor = new CANSparkMax(IntakeExtendConstants.CANID1, MotorType.kBrushless);
-//     slaveMotor = new CANSparkMax(IntakeExtendConstants.CANID2, MotorType.kBrushless);
-//     limitSwitch1 = new DigitalInput(IntakeExtendConstants.kLimitSwitch1);
-//     limitSwitch2 = new DigitalInput(IntakeExtendConstants.kLimitSwitch2);
+  public IntakeExtendSubsystem() {
+    masterMotor = new CANSparkMax(IntakeExtendConstants.CANID1, MotorType.kBrushless);
+    // limitSwitch1 = new DigitalInput(IntakeExtendConstants.kLimitSwitch1);
+    // limitSwitch2 = new DigitalInput(IntakeExtendConstants.kLimitSwitch2);
 
-//     slaveMotor.follow(masterMotor);
-//     slaveMotor.setInverted(true);
-//   }
 
-//   public void extend() {
-//     masterMotor.set(IntakeExtendConstants.kFullPower);
-//   }
+  }
 
-//   public void retract() {
-//     masterMotor.set(-IntakeExtendConstants.kFullPower);
-//   }
+  public void extend() {
+    masterMotor.set(IntakeExtendConstants.kFullPower);
+  }
+
+  public void retract() {
+    masterMotor.set(-IntakeExtendConstants.kFullPower);
+  }
   
-//   public void stopRunning() {
-//     masterMotor.set(IntakeExtendConstants.kZeroPower);
-//   }
+  public void stopRunning() {
+    masterMotor.set(IntakeExtendConstants.kZeroPower);
+  }
 
 //   public boolean isExtended() {
 //     return limitSwitch1.get();
@@ -50,38 +47,38 @@
 //     return limitSwitch2.get();
 //   }
 
-//   /**
-//    * Example command factory method.
-//    *
-//    * @return a command
-//    */
-//   public CommandBase exampleMethodCommand() {
-//     // Inline construction of command goes here.
-//     // Subsystem::RunOnce implicitly requires `this` subsystem.
-//     return runOnce(
-//         () -> {
-//           /* one-time action goes here */
-//         });
-//   }
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public CommandBase exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
 
   
-//   /**
-//    * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-//    *
-//    * @return value of some boolean subsystem state, such as a digital sensor.
-//    */
-//   public boolean exampleCondition() {
-//     // Query some boolean state, such as a digital sensor.
-//     return false;
-//   }
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
 
-//   @Override
-//   public void periodic() {
-//     // This method will be called once per scheduler run
-//   }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 
-//   @Override
-//   public void simulationPeriodic() {
-//     // This method will be called once per scheduler run during simulation
-//   }
-// }
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
