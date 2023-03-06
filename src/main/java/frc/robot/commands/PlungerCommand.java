08bca3545e259190b953b89fd894923f86f7f4c8
Oscar Rosas <oscarsr992@gmail.com>
2023-03-06 17:00:39 -0500

    spell changing plunger subsystem

--- src/main/java/frc/robot/commands/PlungerCommand.java
+++ src/main/java/frc/robot/commands/PlungerCommand.java
@@ -2,19 +2,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClamperSubsystem;
-import frc.robot.subsystems.PlungerSubsystem;
+import frc.robot.subsystems.Plungersubsystem;
 
 public class PlungerCommand extends CommandBase{
-    private final PlungerSubsystem m_PlungerSubsystem;
+    private final Plungersubsystem m_Plungersubsystem;
 
-    public PlungerCommand(PlungerSubsystem subsystem) {
-        m_PlungerSubsystem = subsystem;
+    public PlungerCommand(Plungersubsystem subsystem) {
+        m_Plungersubsystem = subsystem;
         addRequirements(subsystem);
     }
 
     @Override
     public void initialize() {
-        m_PlungerSubsystem.togglePlunger();
-        System.out.println(m_PlungerSubsystem.isEngaged());
+        m_Plungersubsystem.togglePlunger();
+        System.out.println(m_Plungersubsystem.isEngaged());
     }
 }
