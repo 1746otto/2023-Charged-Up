1b441cb8101af1a0d9f5ccb7a1ed56c639f18a5e
Oscar Rosas <oscarsr992@gmail.com>
2023-03-08 16:42:23 -0500

    fixed plunger subsystem

--- src/main/java/frc/robot/subsystems/ClamperSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClamperSubsystem.java
@@ -1,17 +1,13 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Constants.ClamperConstants;
-import frc.robot.Constants.RobotConstants;
 
 public class ClamperSubsystem extends SubsystemBase {
     private final Solenoid piston;
@@ -21,7 +17,7 @@ public class ClamperSubsystem extends SubsystemBase {
     }
 
     
-    public boolean isEngaged() {
+    public boolean isEngaged(){
         return piston.get();
     }
     public void closeClamper(){
