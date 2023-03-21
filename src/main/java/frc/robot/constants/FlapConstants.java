5daa0aa4b282fdd94c8e3fddf6e0b93b81cd6472
CaptenV <vihaan.mangalam@gmail.com>
2023-03-21 16:36:32 -0400

    deleted subsystems

--- src/main/java/frc/robot/constants/FlapConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.constants;
-
-public final class FlapConstants {
-  public static final int kChannel = 10;
-  public static final boolean kFlapClosed = false;
-  public static final boolean kFlapOpen = true;
-}
