b0b1f5fddadb53807c7318bef38b3e101af09b29
Oscar Rosas <oscarsr992@gmail.com>
2023-03-21 20:12:57 -0400

    commiting chnages that weren't initially committed

--- src/main/java/frc/robot/subsystems/ArmPositionSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmPositionSubsystem.java
@@ -1,17 +1,34 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import frc.robot.constants.ArmConstants;
 
-public class ArmPositionSubsystem {
+public class ArmPositionSubsystem extends SubsystemBase {
   // TODO: Change data types to correct types
-  private int armMotor;
-  private int armEncoder;
+  private TalonFX armMotor;
+  private double armEncoder;
 
-  public ArmPositionSubsystem() {}
+  public ArmPositionSubsystem() {
+    armMotor = new TalonFX(ArmConstants.kArmPosMotorID);
+    armMotor.configStatorCurrentLimit(null);
+  }
 
-  public void armToIntake() {}
+  public void armToIntakeOrScore() {
+    armMotor.set(TalonFXControlMode.Position, ArmConstants.kArmIntakeAndScorePos);
+  }
 
-  public void armToRest() {}
+  public void armToRest() {
+    armMotor.set(TalonFXControlMode.Position, ArmConstants.kArmRestPos);
+  }
 
-  public void armToScore() {}
+  public double getArmEncoderValues() {
+    return armEncoder;
+  }
+
+  @Override
+  public void periodic() {
+    armEncoder = armMotor.getSelectedSensorPosition();
+  }
 }
--- src/main/java/frc/robot/subsystems/ArmRollersSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmRollersSubsystem.java
@@ -1,16 +1,27 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import frc.robot.constants.ArmConstants;
 
-public class ArmRollersSubsystem {
+public class ArmRollersSubsystem extends SubsystemBase {
   // TODO: Change data types to correct types
-  private int armRollerMotor;
+  private TalonFX armRollerMotor;
 
-  public ArmRollersSubsystem() {}
+  public ArmRollersSubsystem() {
+    armRollerMotor = new TalonFX(ArmConstants.kArmRollerMotorID);
+  }
 
-  public void armRollerIntake() {}
+  public void armRollerIntake() {
+    armRollerMotor.set(TalonFXControlMode.PercentOutput, ArmConstants.kRollerOutputSpeed);
+  }
 
-  public void armRollerOuttake() {}
+  public void armRollerOuttake() {
+    armRollerMotor.set(TalonFXControlMode.PercentOutput, -ArmConstants.kRollerOutputSpeed);
+  }
 
-  public void armRollerStop() {}
+  public void armRollerStop() {
+    armRollerMotor.set(TalonFXControlMode.PercentOutput, 0);
+  }
 }
