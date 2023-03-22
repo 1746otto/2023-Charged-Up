4a94854bab64cbe06eb9c9154749ec0f37ee99dc
Oscar Rosas <oscarsr992@gmail.com>
2023-03-22 17:04:52 -0400

    trying to mimic swerve set position for arm

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -2,11 +2,13 @@ package frc.robot.constants;
 
 public final class ArmConstants {
   // TODO: change values to accurate positions
-  public static final double kArmIntakeAndScorePos = 0.0;
   public static final double kArmRestPos = 0.0;
+  public static final double kArmIntakeAndScorePos = 20.0;
   public static final double kArmP = .01;
   public static final double kRollerOutputSpeed = 0.5;
+  public static final int kArmGearRatio = 0;
   // TODO: change ids to real nums
   public static final int kArmPosMotorID = 0;
   public static final int kArmRollerMotorID = 1;
+  public static final int kCANCoderID = 3;
 }
