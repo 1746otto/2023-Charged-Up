7e81f6c6ccd7ae1de69c5357f24fdedc33c9a117
Oscar Rosas <oscarsr992@gmail.com>
2023-03-21 20:12:20 -0400

    made subsystems for arm and rollers

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -2,11 +2,11 @@ package frc.robot.constants;
 
 public final class ArmConstants {
   // TODO: change values to accurate positions
-  public static final double kArmConeIntakePos = 0.0;
-  public static final double kArmCubeIntakePos = 0.0;
+  public static final double kArmIntakeAndScorePos = 0.0;
   public static final double kArmRestPos = 0.0;
-  public static final double kArmScorePos = 0.0;
   public static final double kArmP = .01;
+  public static final double kRollerOutputSpeed = 0.5;
   // TODO: change ids to real nums
-  public static final int kArmMotorID = 0;
+  public static final int kArmPosMotorID = 0;
+  public static final int kArmRollerMotorID = 1;
 }
