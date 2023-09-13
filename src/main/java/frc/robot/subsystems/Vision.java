dc3b9f40364f5a4934601fad86cb2d9f9f8d8e01
programming1 <programming@team1746.com>
2023-09-13 18:24:51 -0400

    broken laptop

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,8 +1,16 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -10,8 +18,21 @@ public class Vision {
   static PhotonCamera turretCamera;
   static PhotonPipelineResult lastResult;
   static PhotonTrackedTarget bestTarget;
+  private Pose3d lottoEstimatedRobotPose = new Pose3d();
+  private Pose3d rottoEstimatedRobotPose = new Pose3d();
+  static Pose3d robotEstimatedPose = new Pose3d();
+  private AprilTagFieldLayout tags;
+  // +X is forward, Y is left and right, +Z is up
+  public static Transform3d robotToLotto =
+      new Transform3d(new Translation3d(0.5, 0.5, 0.5), new Rotation3d(0, -Math.PI / 9.0, -29.8));
+  public static Transform3d robotToRotto =
+      new Transform3d(new Translation3d(0.5, -0.5, 0.5), new Rotation3d(0, -Math.PI / 9.0, 29.8));
 
   public Vision() {
+    try {
+    tags = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+    }
+    catch(Exception e) {};
     turretCamera = new PhotonCamera("rotto");
     new Thread(() -> {
       while (true) {
@@ -24,7 +45,12 @@ public class Vision {
                   + Double.toString(
                       bestTarget.getBestCameraToTarget().getRotation().getY() / Math.PI * 180)
                   + ", " + Double.toString(
-                      bestTarget.getBestCameraToTarget().getRotation().getZ() / Math.PI * 180));
+                      bestTarget.getBestCameraToTarget().
+                      getRotation().getZ() / Math.PI * 180));
+          for (PhotonTrackedTarget target : Vision.lastResult.targets) {
+            target.getBestCameraToTarget();
+          }
+        PhotonPoseEstimator poseEstimator = new PhotonPoseEstimator(null, null, turretCamera, null);
         try {
           Thread.sleep(10);
         } catch (InterruptedException e) {
