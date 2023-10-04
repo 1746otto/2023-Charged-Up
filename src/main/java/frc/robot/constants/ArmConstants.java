e152a007cb8da54393293554f1b8931cd3f82178
Oscar Rosas <oscarsr992@gmail.com>
2023-10-04 17:52:43 -0400

    commenting new cancoder values

--- src/main/java/frc/robot/constants/ArmConstants.java
+++ src/main/java/frc/robot/constants/ArmConstants.java
@@ -3,11 +3,12 @@ package frc.robot.constants;
 public final class ArmConstants {
   // TODO: change values to accurate positions and values
   // TODO: chnage positions to match cancoder.
-  public static final double kArmRestPos = 0.0;
-  public static final double kArmIntakeAndScorePos = -18640.0;
+  public static final double kArmRestPos = 0.0; // 0.5
+  public static final double kArmIntakeAndScorePos = -18640.0; // CANCoder: 0.9065 human player pos:
+                                                               // 0.5634
   // TODO: change cube intake pos
   // TODO: change positions constants to match cancoder.
-  public static final double kArmConeIntakePos = -18640.0;
+  public static final double kArmConeIntakePos = -18640.0; // 0.7521
   public static final double kArmHighScoringPos = -14776.0;
   public static final double kArmP = .4;
   public static final double kRollerSpeed = 0.5;
