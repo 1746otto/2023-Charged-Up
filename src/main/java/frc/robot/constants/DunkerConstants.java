5a887a57a69eb7022c9dad977cd8e7f5963b012b
Oscar Rosas <oscarsr992@gmail.com>
2023-10-25 16:31:44 -0400

    new dunk constant

--- src/main/java/frc/robot/constants/DunkerConstants.java
+++ src/main/java/frc/robot/constants/DunkerConstants.java
@@ -3,7 +3,7 @@ package frc.robot.constants;
 public final class DunkerConstants {
   public static final int kCANCoderID = 30;
   public static final double KDunkSpeed = -0.5;
-  public static final double KRDunkSpeed = 0.5;
+  public static final double KRDunkSpeed = 0.35;
   public static final double KZeroPos = 0;
   public static final double KDownPosition = -5.16660;
   public static final double KMaxDownCurrent = 50;
